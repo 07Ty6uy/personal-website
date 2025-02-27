2a62acbb979d3ca9ceab97520881822d023415cf
07Ty6uy <163577969+07Ty6uy@users.noreply.github.com>
2025-02-27 14:31:56 -0500

    Added weather info files
    Added html, and javascript files that allows the user to find weather in a city of their choosing. Also updated css file to add some styles to fit this page, as well as update the header.

---
+++ weather.js
@@ -0,0 +1,46 @@
+async function fetchWeather() {
+    const apiKey = 'b57b2a2ca51a41b12b77b0d4781281dd';
+
+    const cityInp = document.getElementById('cityInput').value;
+    if (cityInp) {
+        url = `http://api.weatherstack.com/current?access_key=${apiKey}&query=${cityInp}`;
+    }
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Could not fetch weather data');
+        }
+        const data = await response.json();
+        const weather = data.current;
+
+        const temp = weather.temperature;
+        const feelsLike = weather.feelslike;
+        const state = data.location.region;
+        const country = data.location.country;
+        const desc = weather.weather_descriptions[0];
+        const icon = weather.weather_icons[0];
+
+        const cityText = document.getElementById('city');
+        const tempText = document.getElementById('temp');
+        const feelsLikeText = document.getElementById('feelsLike');
+        const descriptionText = document.getElementById('descriptions');
+        const imgElement = document.getElementById('icon');
+
+        cityText.textContent = `${cityInp}  ${state}, ${country}`;
+        tempText.textContent = `Temp: ${temp}°C (${toFerh(temp)}°F)`;
+        feelsLikeText.textContent = `Feels like: ${feelsLike}°C (${toFerh(feelsLike)}°F)`;
+        descriptionText.textContent = `Description: ${desc}`;
+        imgElement.src = icon;
+        imgElement.style.display = 'block';
+        imgElement.style.marginLeft = 10 + 'px';
+
+        console.log(data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function toFerh(temp) {
+    return (temp * 9/5) + 32;
+}
